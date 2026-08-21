d5b062f4f67a2f152546f8e3feb9980b9c54cbf5
Pinto Manuel <pintoydc@gmail.com>
2026-08-21 09:48:05 +0200

    Scaffold ASP.NET Core MVC application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WrapPasswordAssessment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
